611bb5da9e1f94cc4bacc3d3cee6a8fa1e64c3cf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-30 01:54:58 +0000

    unity-hub 3.20.0

--- Casks/u/unity-hub.rb
+++ Casks/u/unity-hub.rb
@@ -1,9 +1,9 @@
 cask "unity-hub" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.19.5"
-  sha256 arm:   "da6773add1f638d8bd00615a47661408d1de4a3c94e7efb5a37f1e7131e63272",
-         intel: "d86dc505d27560dfe3b5f6da060093034c0ecf934e6d7e85de3223947ef82702"
+  version "3.20.0"
+  sha256 arm:   "1d3bed9cbc36b813921b68a4bb63ebfd97c8e054833531529b53f9c50b003aa7",
+         intel: "0a7174318f270cf41eee9ec641c770123947644bbf4a8af6969529fecc41dddb"
 
   url "https://public-cdn.cloud.unity3d.com/hub/prod/#{version}/UnityHubSetup-#{version}-#{arch}.dmg"
   name "Unity Hub"
